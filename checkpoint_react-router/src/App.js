5123b794604865b7bc18f5cc32e0f8bc39e36523
Nemesicode <abdoulayediallomail@gmail.com>
2021-12-09 17:58:59 +0000

    first commit

---
+++ checkpoint_react-router/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import MovieApp from './component/MovieApp';
+
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <div>
+        <MovieApp/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
